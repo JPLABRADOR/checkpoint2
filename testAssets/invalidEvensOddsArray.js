629837c87658c09b435e51e5c84da37d1c2e4edd
JPLABRADOR <vanhagarsux@gmail.com>
2020-07-29 14:51:27 -0700

    Checkpoint 2 Project Data Files

--- testAssets/invalidEvensOddsArray.js
+++ testAssets/invalidEvensOddsArray.js
@@ -13,5 +13,15 @@ module.exports = [
         input: "1+++++p",
         evenOutput: "Evens: [null]",
         oddOutput: "Odds: [null]"
+    },
+    {
+        input: "4/",
+        evenOutput: "Evens: [null]",
+        oddOutput: "Odds: [null]"
+    },
+    {
+        input: "4.....",
+        evenOutput: "Evens: [null]",
+        oddOutput: "Odds: [null]"
     }
 ]
